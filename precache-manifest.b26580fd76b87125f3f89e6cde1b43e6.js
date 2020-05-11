b4978f2228064052407dc9f00dc5937749238e43
JACK <zhenya271@gmail.com>
2020-05-12 01:42:50 +0300

    adding build

---
+++ precache-manifest.b26580fd76b87125f3f89e6cde1b43e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e14c6f70ce1004cd4351a8563ff50ef5",
+    "url": "/UsersApp/index.html"
+  },
+  {
+    "revision": "67578d1371f81cc1351e",
+    "url": "/UsersApp/static/css/main.86464f0b.chunk.css"
+  },
+  {
+    "revision": "3b2e661d416334d028f0",
+    "url": "/UsersApp/static/js/2.2a460a0b.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/UsersApp/static/js/2.2a460a0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67578d1371f81cc1351e",
+    "url": "/UsersApp/static/js/main.8a9c33fd.chunk.js"
+  },
+  {
+    "revision": "1acccce7f5a6968a40bf",
+    "url": "/UsersApp/static/js/runtime-main.65da5eec.js"
+  }
+]);
